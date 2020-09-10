eea44d813d8f12527271a4c4cbdfb98e4e042d47
aleg001 <60187741+aleg001@users.noreply.github.com>
2020-09-09 21:35:16 -0600

    Avances del proyecto
    Trabajo realizado durante la primera semana de septiembre

---
+++ proyecto.java
@@ -0,0 +1,15 @@
+/***************************************************
+Proyecto semestral
+Título: MPAAA
+Autor: Alejandro Gómez
+Carné 20347
+Fecha: 30/08/2020
+Ultima modificacion: 2/09/2020
+					
+Driver program que desarrolla el simulador de 
+supermercado
+***************************************************/
+
+public class proyecto {
+    
+}
